b4e029e2267440f5a6a8cef06e18ba8b68412516
Nikita Sharma <nikitasharma@Nikitas-MacBook-Air.local>
2026-06-10 13:58:43 +0530

    Update CORS with exact Vercel URL

--- server/index.js
+++ server/index.js
@@ -2,6 +2,6 @@ app.use(cors({
   origin: [
     'http://localhost:5173',
     'http://localhost:5174',
-    'https://goal-tracker-app.vercel.app'
+    'https://goal-tracker-h449vgmx0-nikitasharma5078s-projects.vercel.app'
   ]
 }));
